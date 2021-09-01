c474f832d0fb0c3c39ef24cee3a014ef3e07047f
Keira <KeiraJCoder@hotmail.com>
2021-09-01 11:46:32 +0100

    commit

--- Day2/variables.py
+++ Day2/variables.py
@@ -1,6 +1,20 @@
 my_name = "Keira"
+fav_drink = "Monster"
 my_age = 34
-fav_drink = "monster"
 
-print(f"My name is {my_name}. I am {my_age} years old, and my favourite drink is {fav_drink}")
 
+print(my_name)
+
+print("My favourite drink is", fav_drink)
+
+print("My favourite drink is " + fav_drink)
+# No space between the string and variable
+
+# print("My age is " + my_age)
+# Concatenate / Concatenation is to add multiple things together
+
+# print("My name is " + my_name + " and I am " + my_age + " years old. My favourite drink is " + fav_drink + ".")
+
+print("My name is {}. My age is {}.".format(my_name, my_age))
+
+print(f"My name is {my_name}. I am {my_age} years old and my favourite drink is {fav_drink}.")
