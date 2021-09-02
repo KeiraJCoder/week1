8e1eb53c723e3e5d227878909937e81693c55f22
Keira <KeiraJCoder@hotmail.com>
2021-09-02 11:11:06 +0100

    commit

--- Day2/homework.py
+++ Day2/homework.py
@@ -1,56 +1,62 @@
-# # Task 1
-# # create a programme to show your name, your age and your favourite colour
+# Task 1
+# create a programme to show your name, your age and your favourite colour
 
 
-# my_name = ("Keira".capitalize())
-# my_age =33
-# fav_colour = "Purple"
+my_name = ("Keira".capitalize())
+my_age =33
+fav_colour = "Purple"
 
-# print (my_name, my_age, fav_colour)
-# print (f"my name is {my_name}, my age is {my_age}, and my favourite colour is {fav_colour}" .capitalize())
-
-
-
-# # Task 2
-# # write a programme that prints what you're having for breakfast, lunch and dinner
-# # then update those variables and print the updated 
-
-# breakfast = "pancakes"
-# lunch = "tuna sandwich"
-# dinner = "chicken curry"
-
-# print(f"For breakfast I had {breakfast} and then for lunch I had {lunch} and then for dinner I had {dinner}")
-
-# breakfast = "cereal"
-# lunch = "jacket potato"
-# dinner = "spaghetti bolognese"
-
-# print(f"For breakfast I had {breakfast} and then for lunch I had {lunch} and then for dinner I had {dinner}")
+print (my_name, my_age, fav_colour)
+print (f"my name is {my_name}, my age is {my_age}, and my favourite colour is {fav_colour}" .capitalize())
 
 
-# # Task 3
-# # Create 9 variables, space1, space2, space3 etc
-# # assign either the value of 'x' or 'o' 
 
-# space1 = "x"
-# space2 = "o"
-# space3 =  ""
-# space4 = "x"
-# space5 = "x"
-# space6 = ""
-# space7 = "o"
-# space8 = ""
-# space9 = ""
+# Task 2
+# write a programme that prints what you're having for breakfast, lunch and dinner
+# then update those variables and print the updated 
 
-# print ("   |   |  ")
-# print (f" {space1} | {space2} | {space3} ")
-# print ("   |   |  ")
-# print("-----------")
-# print ("   |   |  ")
-# print (f" {space4} | {space5} | {space6} ")
-# print ("   |   |  ")
-# print("-----------")
-# print ("   |   |  ")
-# print (f" {space7} |{space8}   | {space9} ")
-# print ("   |   |  ")
+breakfast = "pancakes"
+lunch = "tuna sandwich"
+dinner = "chicken curry"
 
+print(f"For breakfast I had {breakfast} and then for lunch I had {lunch} and then for dinner I had {dinner}")
+
+breakfast = "cereal"
+lunch = "jacket potato"
+dinner = "spaghetti bolognese"
+
+print(f"For breakfast I had {breakfast} and then for lunch I had {lunch} and then for dinner I had {dinner}")
+
+
+# Task 3
+# Create 9 variables, space1, space2, space3 etc
+# assign either the value of 'x' or 'o' 
+
+space1 = "x"
+space2 = "o"
+space3 =  ""
+space4 = "x"
+space5 = "x"
+space6 = ""
+space7 = "o"
+space8 = ""
+space9 = ""
+
+print ("   |   |  ")
+print (f" {space1} | {space2} | {space3} ")
+print ("   |   |  ")
+print("-----------")
+print ("   |   |  ")
+print (f" {space4} | {space5} | {space6} ")
+print ("   |   |  ")
+print("-----------")
+print ("   |   |  ")
+print (f" {space7} |{space8}   | {space9} ")
+print ("   |   |  ")
+
+from datetime import date
+
+date1 = date(2021, 9, 2)
+date2 = date(1987, 8, 1)
+i = date1 - date2
+print(i.days)
